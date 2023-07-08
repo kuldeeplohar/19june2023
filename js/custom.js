4280851da80ab93d8d9bb1ee0210c6d280151e67
kuldeeplohar <loharkuldeep169@gmail.com>
2023-07-08 17:36:42 +0530

    f

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+
+    //property.value
+var student1={
+    fname:"kuldeep",
+    lname:'lohar',
+    address:"sarwaniya maharaj",
+    education:"BA",
+  
+   
+}
+  console.log(student1.fname)
+  console.log(student1.lname)
+  console.log(student1.address)
+  console.log(student1['education'])
+
+
+let student2={
+  fname:'rohit',
+  lname:'mali',
+  address:"neemuch",
+  education:'BCA',
+
+  getFullDetails(){
+    console.log(student2.fname)
+  }
+  }
+  
+  console.log(student2.fname)
+  
+
+
+
+  
